62b3cd1e7e1d157c9468d3013073603527a4d50c
Kinley Ramson <kinleyramson@gmail.com>
2017-08-08 19:35:45 -0700

    finished the javascript!

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 var counter = 0;
 
+var name = prompt('What\'s your name, buddy?');
+console.log();
+
 alert('WELCOME TO THE GAME. Don\'t worry, it\'s not a very complicated game. The game is called FOUR TRUTHS AND A LIE. I\'m going to tell you something that I, Kinley, have done or experienced at some point in my life....but one of things is going to be a lie. Which one\'s the lie? Well that\'s up to you to figure out. Are you ready to play? The rules are simple, you just answer yes if you think what I said is a truth, and no is you think it\'s the lie. Remember the name of the game is FOUR TRUTHS AND A LIE and I *will* be keeping score, so if you keep marking no, I\'m gonna know you suck, and tell you so. Let\'s begin! (If you think you can hack it.)');
 
 var chinook = prompt('I have ridden in a military Chinook helicopter with my dog. (Remember, yes for truth, no for lie.)');
@@ -21,9 +24,61 @@ states = states.toUpperCase();
 console.log('q2: states y/n (y)');
 console.log(states);
 
-if(states === 'Y' || states || 'YES') {
+if(states === 'Y' || states === 'YES') {
   alert('Alright, maybe you\'re not a complete waste of brainspace. I guess it\'s not *that* unbelievable for someone to have been to 46 states. I did hitchhike most of that, though, betcha didn\'t know that! Next question!');
   counter ++;
 } else {
-  alert('HAHA YOU SUCK! I mean...WRONG! I mean...Yeah, no, I really have. I even hitchhiked most of that! Anyway, you can still get the next one right (maybe, I doubt it, though.)');
+  alert('HAHA YOU SUCK! I mean...WRONG! I mean...Yeah, no, I really have been to 46 states. I even hitchhiked most of that! Anyway, you can still get the next one right (maybe, I doubt it, though.)');
+}
+
+var canyon = prompt('Here\'s a good one for ya: I\'ve ridden a burro to the bottom of the Grand Canyon (yes for truth, no for lie)');
+canyon = canyon.toUpperCase();
+console.log('q3: canyon y/n (n)');
+console.log(canyon);
+
+if(canyon === 'N' || canyon === 'NO') {
+  alert('Well, well, well. You\'re smarter than the average bear. I\'ve never even *been* to the Grand Canyon. (though I have been to the Grand Canyon of the Yellowstone [I\'ll link to that on the page if you\'ve never heard of it.]) On to the next! You have two questions left.');
+  counter ++;
+} else {
+  alert('HAH! That was the lie! Assuming I told you the truth in the first place and there\'s only one lie. But I haven\'t even been to the Grand Canyon at all.(though I have been to the Grand Canyon of the Yellowstone [I\'ll link to that on the page if you\'ve never heard of it.]) On to the next! You have two questions left.');
+}
+
+var bear = prompt('Speaking of bears, this one time, driving from Alaska south through Canada, I totaled a 3/4 ton pickup truck on a grizzly bear. (yes for truth, no for lie)');
+bear = bear.toUpperCase();
+console.log('q4: bear y/n (y)');
+console.log(bear);
+
+if(bear === 'Y' || bear === 'YES') {
+  alert('Yeah yeah yeah, you probably heard me tell that story before, or are friends with me on facebook or something. I ought to bive half credit for that one. But yeah, apparently near Destruction Bay is the highest concentration of grizzly bears in North America. No Shit! Scared the crap out of the dog, that\'s for sure. Had to hitchhike all the way back to Talkeetna. Last question coming up!');
+  counter++;
+} else {
+  alert('You\'re not too bright, are you? I even foreshadowed that shit. Oh well. You can\'t help what don\'t want helped. Last question coming up. Good luck (you\'re gonna need it.)');
+}
+
+var salmon = prompt('Last question! Staying on theme...I once caught a 40lb King salmon (that\'s a Chinook salmon, just to throw in a little call-back) (yes for truth, no for lie)');
+salmon = salmon.toUpperCase();
+console.log('q5: salmon y/n (y)');
+console.log(salmon);
+
+if(salmon === 'Y' || salmon === 'YES') {
+  counter++;
+}
+
+if(counter === 5) {
+  alert('Holy crap, ' + name + '! What are you?! A stalker?! You got 5/5 correct! How did you know so damn much about me? I don\'t know if you\'re preternaturally good at guessing games, have *really* good luck, or you\'ve been playing this game over and over until you got them all right. Whatever it is, I\'m creeped out. But I guess you can look at the page anyway and see some pictures and stuff of me in the Last Frontier. Have fun creep!');
+} else if (counter === 3 || counter === 4) {
+  ('Not too shabby, ' + name + '. I grudgingly concede that you\'re not complete shit at guessing games (or you cheated and played more than once). You got ' + counter + '/5 correct. That\'s not the worst score I\'ve ever seen. You may not feast your eyes on pictures and information of yours truly in the Last Frontier.');
+} else if (counter === 2 || counter === 1) {
+  alert('Oh come ON. IS that *really* the best you can do? ' + counter + '/5? That\'s pathetic. I almost don\'t even want you to see my page, but I guess you can...It\'s a bunch of awesome pictures and stuff about me in the Last Frontier. You definitely don\'t deserve it, but have at it.');
+} else if (counter === 0) {
+  alert('YOU GOT 0. FREAKING ZE-RO. HOW DO YOU EVEN GET SUCH A RIDICULOUS SCORE?');
+  alert('you don\'t know me');
+  alert('you don\'t know me at all');
+  alert('you don\'t know me');
+  alert('you don\'t know me at all');
+  alert('you don\'t know me');
+  alert('you don\'t know me at all');
+  alert('you don\'t know me');
+  alert('you don\'t know me at all');
+  alert('Yeah so I guess even though you don\'t remotely deserve it, you can look at my site, with my awesome pictures and stuff about me in the Last Frontier');
 }
