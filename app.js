5debb169113142204314701dce7936208059b8c5
Kinley Ramson <kinleyramson@gmail.com>
2017-08-09 08:45:26 -0700

    woooooooo donezo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log();
 
 alert('WELCOME TO THE GAME. Don\'t worry, it\'s not a very complicated game. The game is called FOUR TRUTHS AND A LIE. I\'m going to tell you something that I, Kinley, have done or experienced at some point in my life....but one of things is going to be a lie. Which one\'s the lie? Well that\'s up to you to figure out. Are you ready to play? The rules are simple, you just answer yes if you think what I said is a truth, and no is you think it\'s the lie. Remember the name of the game is FOUR TRUTHS AND A LIE and I *will* be keeping score, so if you keep marking no, I\'m gonna know you suck, and tell you so. Let\'s begin! (If you think you can hack it.)');
 
-var chinook = prompt('I have ridden in a military Chinook helicopter with my dog. (Remember, yes for truth, no for lie.)');
+var chinook = prompt('1. I have ridden in a military Chinook helicopter with my dog. (Remember, yes for truth, no for lie.)');
 chinook = chinook.toUpperCase();
 console.log('q1: chinook y/n (y)');
 console.log(chinook);
@@ -19,7 +19,7 @@ if(chinook === 'Y' || chinook === 'YES') {
   alert('Bahahahaha! Nice try big shot! I HAVE been in a Chinook with my dog, when I worked for a bush plane company in Alaska, sending climbers up to base camp on Denali, and got to go up myself a few times in the Chinooks the National Guard uses to help get camp ready for the season. Better luck next time, dweeb! NEXT QUESTION');
 }
 
-var states = prompt('OK, try this one on for size: I have been to 46 of the United States (yes for truth, no for lie)');
+var states = prompt('2. OK, try this one on for size: I have been to 46 of the United States (yes for truth, no for lie)');
 states = states.toUpperCase();
 console.log('q2: states y/n (y)');
 console.log(states);
@@ -31,7 +31,7 @@ if(states === 'Y' || states === 'YES') {
   alert('HAHA YOU SUCK! I mean...WRONG! I mean...Yeah, no, I really have been to 46 states. I even hitchhiked most of that! Anyway, you can still get the next one right (maybe, I doubt it, though.)');
 }
 
-var canyon = prompt('Here\'s a good one for ya: I\'ve ridden a burro to the bottom of the Grand Canyon (yes for truth, no for lie)');
+var canyon = prompt('3. Here\'s a good one for ya: I\'ve ridden a burro to the bottom of the Grand Canyon (yes for truth, no for lie)');
 canyon = canyon.toUpperCase();
 console.log('q3: canyon y/n (n)');
 console.log(canyon);
@@ -43,7 +43,7 @@ if(canyon === 'N' || canyon === 'NO') {
   alert('HAH! That was the lie! Assuming I told you the truth in the first place and there\'s only one lie. But I haven\'t even been to the Grand Canyon at all.(though I have been to the Grand Canyon of the Yellowstone [I\'ll link to that on the page if you\'ve never heard of it.]) On to the next! You have two questions left.');
 }
 
-var bear = prompt('Speaking of bears, this one time, driving from Alaska south through Canada, I totaled a 3/4 ton pickup truck on a grizzly bear. (yes for truth, no for lie)');
+var bear = prompt('4. Speaking of bears, this one time, driving from Alaska south through Canada, I totaled a 3/4 ton pickup truck on a grizzly bear. (yes for truth, no for lie)');
 bear = bear.toUpperCase();
 console.log('q4: bear y/n (y)');
 console.log(bear);
@@ -55,13 +55,15 @@ if(bear === 'Y' || bear === 'YES') {
   alert('You\'re not too bright, are you? I even foreshadowed that shit. Oh well. You can\'t help what don\'t want helped. Last question coming up. Good luck (you\'re gonna need it.)');
 }
 
-var salmon = prompt('Last question! Staying on theme...I once caught a 40lb King salmon (that\'s a Chinook salmon, just to throw in a little call-back) (yes for truth, no for lie)');
+var salmon = prompt('5. Last question! Staying on theme...I once caught a 40lb King salmon (that\'s a Chinook salmon, just to throw in a little call-back) (yes for truth, no for lie)');
 salmon = salmon.toUpperCase();
 console.log('q5: salmon y/n (y)');
 console.log(salmon);
 
 if(salmon === 'Y' || salmon === 'YES') {
   counter++;
+} else {
+  counter = counter;
 }
 
 if(counter === 5) {
